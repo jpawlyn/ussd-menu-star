3a3496829bb5e713dd9dad2d473e0e0a3237725f
Jolyon Pawlyn <jpawlyn@gmail.com>
2025-04-11 09:28:34 +0200

    Attempt to fix CI timeout error
    Ferrum::ProcessTimeoutError:
           Browser did not produce websocket url within 15 seconds, try to increase :process_timeout

--- spec/support/cuprite.rb
+++ spec/support/cuprite.rb
@@ -4,7 +4,8 @@ Capybara.javascript_driver = :cuprite
 Capybara.register_driver(:cuprite_custom) do |app|
   headless = ENV["HEADLESS"].blank? || ENV["HEADLESS"]&.downcase == "true"
   Capybara::Cuprite::Driver.new(
-    app, process_timeout: 15, window_size: [ 1400, 900 ], inspector: ENV["INSPECTOR"], headless:
+    app, browser_options: { "no-sandbox": nil }, process_timeout: 20, window_size: [ 1400, 900 ],
+    inspector: ENV["INSPECTOR"], headless:
   )
 end
 
