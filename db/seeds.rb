2e56d4f20a27ee93442dc76e15798bc048947984
Jolyon Pawlyn <jpawlyn@gmail.com>
2025-04-23 13:59:09 +0200

    Use a seeds file instead of fixtures
    since this is the standard Rails way of populating a development or production db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+COUNTRIES = [
+  { name: 'Ghana', country_code: 'GHA', short_name: 'testing' },
+  { name: 'Kenya', country_code: 'KEN', short_name: 'testing' },
+  { name: 'Malawi', country_code: 'MWI', short_name: 'testing' },
+  { name: 'Namibia', country_code: 'NAM', short_name: 'testing' },
+  { name: 'Nigeria', country_code: 'NGA', short_name: 'testing' },
+  { name: 'Rwanda', country_code: 'RWA', short_name: 'testing' },
+  { name: 'South Africa', country_code: 'ZAF', short_name: 'testing' },
+  { name: 'Tanzania', country_code: 'TZA', short_name: 'testing' },
+  { name: 'Uganda', country_code: 'UGA', short_name: 'testing' },
+  { name: 'Zambia', country_code: 'ZMB', short_name: 'testing' }
+]
+
+COUNTRIES.each do |country_hash|
+  service_code = ServiceCode.find_or_create_by!(country_hash)
+  service_code.accounts.find_or_create_by!(name: "#{country_hash[:name]} test account")
+end
+
+# Create a default admin user
+user = User.find_or_initialize_by(email_address: 'admin@example.com')
+if user.new_record?
+  user.password = 'pleasechangeme'
+  user.save!
+end
